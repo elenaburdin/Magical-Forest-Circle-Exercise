d0a15b0b72c42325e35ee3a442797fd5c1d951d1
elenaburdin <66010914+elenaburdin@users.noreply.github.com>
2023-01-24 13:16:58 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+const containerCenter = document.querySelector('#container');
+containerCenter.style.textAlign = 'center';
+
+const imageForest = document.querySelector('img');
+imageForest.style.width = '150px';
+imageForest.style.borderRadius = '50%';
+
+
+
+
